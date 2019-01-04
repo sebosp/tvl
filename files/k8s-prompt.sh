864696582472d635ee3144ad82877dc59f2c64d1
Seb Ospina <kraige@gmail.com>
2019-01-04 18:23:56 +0100

    Fixing typo on k8s prompt ttl

--- files/k8s-prompt.sh
+++ files/k8s-prompt.sh
@@ -2,13 +2,13 @@ __k8s_prompt ()
 {
   # Use this var to control display of the AWS prompt.
   if [[ "1" == "${K8S_PS1_SHOW}" ]]; then
-    if [[ -z "$K8S_PROMT_TTL" ]]; then
-      export K8S_PROMT_TTL=15
+    if [[ -z "$K8S_PROMPT_TTL" ]]; then
+      export K8S_PROMPT_TTL=15
     fi
     if [[ -z "$K8S_LAST_PROMPT_CHECK" ]]; then
       export K8S_LAST_PROMPT_CHECK=$(date +'%s')
     fi
-    if [[ $(($K8S_PROMT_TTL + $K8S_LAST_PROMPT_CHECK)) < $(date +'%s') ]]; then
+    if [[ $(($K8S_PROMPT_TTL + $K8S_LAST_PROMPT_CHECK)) < $(date +'%s') ]]; then
       export K8SCTX=$(kubectl config current-context 2>/dev/null)
       export K8SNS=$(kubectl config view -o json|jq ".contexts | map(select(.name| contains(\"$K8SCTX\")))[0]|.context.namespace" -r|cut -c-10)
       export K8S_LAST_PROMPT_CHECK=$(date +'%s')
